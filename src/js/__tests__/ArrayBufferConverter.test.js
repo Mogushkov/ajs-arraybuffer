df169b2a9b854069306c4a0f738bf5503b63624f
Gapur Mogushkov <gapur@mail.ru>
2021-04-26 18:46:03 +0300

    first commit

--- src/js/__tests__/ArrayBufferConverter.test.js
+++ src/js/__tests__/ArrayBufferConverter.test.js
@@ -1,12 +1,12 @@
 import ArrayBufferConverter from '../ArrayBufferConverter';
 
 test('load()', () => {
-  const buffer = ArrayBufferConverter.load(buffer);
-  expect(ArrayBufferConverter.buffer).toEqual(buffer);
+  const result = ArrayBufferConverter.load('result');
+  expect(ArrayBufferConverter.result).toEqual(result);
 });
 
 test('toString()', () => {
-  const array = ArrayBufferConverter.toString();
+  const array = ArrayBufferConverter.toString('');
   const dataString = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
   expect(array).toBe(dataString);
 });
